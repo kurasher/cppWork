67661cff66f3847afa278ab771b9767844a82255
kurasher <27tonokurasher07@gmail.com>
2020-07-04 22:31:38 +0900

    add practice.cpp

---
+++ Practice/practice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int first, second;
+    cin >> first;
+    cin >> second;
+    cout << first << " + " << second << " = " << first + second << endl;
+    cout << first << " - " << second << " = " << first - second << endl;
+    return 0;
+}
