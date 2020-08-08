a14ee0d6fc1c10bd327b0683e90a7788f406827e
kurasher <27tonokurasher07@gmail.com>
2020-08-09 00:08:34 +0900

    add abc90 q1

---
+++ AtCoderProblems/abc90/DiagonalString.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)n ; i++)
+
+int main(){
+    string a;
+    string b;
+    string c;
+    cin >> a >> b >> c;
+    cout << a[0] << b[1] << c[2] << endl;
+}
