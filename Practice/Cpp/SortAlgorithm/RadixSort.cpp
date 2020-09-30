c2cd7cd9e2a4f8172d09b00682acca98cf20348b
kurasher <27tonokurasher07@gmail.com>
2020-09-30 09:22:44 +0900

    [add] RadixSort.cpp

--- Practice/Cpp/SortAlgorithm/RadixSort.cpp
+++ Practice/Cpp/SortAlgorithm/RadixSort.cpp
@@ -1,27 +1,47 @@
+// Reference URL
+// http://www1.cts.ne.jp/~clab/hsample/Sort/Sort5.html
+
 #include <bits/stdc++.h>
 #include <time.h>
 #include <stdlib.h>
 using namespace std;
 
-void swap(int& a, int& b){
-    int temp = a;
-    a = b;
-    b = temp;
-}
 
-void BubbleSort(vector<int>& list){
-    for(int i = 0; i < list.size(); i++){
-        for(int j = list.size() - 1; j > i; j--){
-            if(list.at(j-1) > list.at(j)){
-                swap(list.at(j-1), list.at(j));
+void RadixSort(vector<int>& list, int N, int R){
+    vector<int> rad(N);//基数をしまう配列
+    vector<int> y(N);//作業用配列
+
+    int i, j, k;
+    int m = 1;//基数を取り出す桁
+
+    while(m <= R){
+        for(i = 0; i < N; i++){
+            rad.at(i) = (list.at(i) / m) % 10;//基数を取り出してrad[i]に保存
+        }
+
+        k = 0;//配列の添え字
+        for(i = 0; i <= 9; i++){
+            for(j = 0; j < N; j++){
+                if(rad.at(j) == i){//基数の小さいモノから
+                    y.at(k) = list.at(j);//配列yにコピー
+                    k++;
+                }
             }
         }
+
+        for(i = 0; i < N; i++){
+            list.at(i) = y.at(i);//コピーし直す
+        }
+
+        m *= 10;//基数を取り出す桁を一つ上げる
     }
+
     return;
 }
 
 int main(){
-    int N = 1;
+    int N = -1;//ソートするデータ数
+    int R = 1000;//ソートするデータから取り出す基数の上限
     cin >> N;
     if(N < 0){
         N = 100;
@@ -38,20 +58,27 @@ int main(){
     }
     cout << endl;
 
-    BubbleSort(intList);
+    RadixSort(intList, N, R);
     sort(ansList.begin(), ansList.end());
 
+    cout << "--RADIX SORT--" << endl;
     for(int num : intList){
         cout << num << " ";
     }
     cout << endl;
 
+    cout << "--ANSER--" << endl;
+    for(int num : intList){
+        cout << num << " ";
+    }
+    cout << endl;
 
-    // int count = 0;
-    // for(int i = 0; i < intList.size(); i++){
-    //     if(intList.at(i) == ansList.at(i)){
-    //         count++;
-    //     }
-    // }
-    // cout << count << endl;
+    //一致した数
+    int count = 0;
+    for(int i = 0; i < intList.size(); i++){
+        if(intList.at(i) == ansList.at(i)){
+            count++;
+        }
+    }
+    cout << count << endl;
 }
