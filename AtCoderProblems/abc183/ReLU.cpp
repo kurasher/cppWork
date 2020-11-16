b3b0aca14ed8dd1a7cf793c5095d80d200793b46
kurasher <27tonokurasher07@gmail.com>
2020-11-16 12:35:35 +0900

    [add] abc182 q2

---
+++ AtCoderProblems/abc183/ReLU.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int X;
+    cin >> X;
+
+    if(X >= 0){
+        cout << X << endl;
+    }else{
+        cout << 0 << endl;
+    }
+}
