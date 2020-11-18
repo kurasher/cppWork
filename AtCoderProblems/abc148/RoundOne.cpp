cd621cbf3f073cc2b4b049fc5ed77a0247330341
kurasher <27tonokurasher07@gmail.com>
2020-11-18 18:31:50 +0900

    [mod] 微修正

--- AtCoderProblems/abc148/RoundOne.cpp
+++ AtCoderProblems/abc148/RoundOne.cpp
@@ -9,7 +9,7 @@ int main(){
         cout << 3 << endl;
     }else if((A == 1 && B == 3) || (B == 1 && A == 3)){
         cout << 2 << endl;
-    }else if((A == 2 && B == 3) || (A == 2 && B == 3)){
+    }else if((A == 2 && B == 3) || (B == 2 && A == 3)){
         cout << 1 << endl;
     }
 }
